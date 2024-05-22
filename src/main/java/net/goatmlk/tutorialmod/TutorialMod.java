4a72d98e9ec2bf4c9e8966d7d6db710f3d8a4f28
Kevin Blimline <kblimy2@gmail.com>
2024-05-22 15:18:48 -0400

    Tutorials 1 and 2 5/22/24

--- src/main/java/net/goatmlk/tutorialmod/TutorialMod.java
+++ src/main/java/net/goatmlk/tutorialmod/TutorialMod.java
@@ -1,6 +1,9 @@
 package net.goatmlk.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.goatmlk.tutorialmod.item.ModCreativeModeTabs;
+import net.goatmlk.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -26,6 +29,10 @@ public class TutorialMod
     public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModCreativeModeTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
@@ -45,7 +52,10 @@ public class TutorialMod
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
